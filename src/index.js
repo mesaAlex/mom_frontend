a5369d0030c36eff50b1db9bfac1d37a06680543
Alex Mesa <mesa.coria@outlook.com>
2026-03-17 09:11:16 -0400

    Big update on JSX Code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path = "/" element = {<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
+  )
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <App />
   </React.StrictMode>
 );
